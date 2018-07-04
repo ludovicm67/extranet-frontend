927ae13b90c73174f4113e406198334c137e6471
ludovicm67 <ludovicmuller1@gmail.com>
2018-07-04 11:19:56 +0200

    added a simple navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import NavBar from './components/NavBar'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Test
+        <NavBar />
       </div>
     );
   }
