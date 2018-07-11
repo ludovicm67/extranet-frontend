7074e7c6bdbc8afbfae9b52ca908b70c616635df
ludovicm67 <ludovicmuller1@gmail.com>
2018-07-11 15:26:47 +0200

    updated endpoint so that it can work in prod and in dev env

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,10 @@
-const API_ENDPOINT = location.protocol + '//api.' + window.location.hostname;
+const navPort = parseInt(window.location.port);
+let apiPort = '';
+if (navPort && navPort !== 0 && navPort !== 80 && navPort !== 443) {
+  apiPort = ':8000';
+}
+
+const API_ENDPOINT = window.location.protocol + '//api.' + window.location.hostname + apiPort;
 
 export default {
   API_ENDPOINT,
