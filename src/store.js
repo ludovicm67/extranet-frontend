936319173d284d76f8239676bc32f112c32aea78
ludovicm67 <ludovicmuller1@gmail.com>
2018-07-19 15:19:12 +0200

    disable logger in prod env

--- src/store.js
+++ src/store.js
@@ -2,9 +2,14 @@ import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './reducers';
 import logger from 'redux-logger';
 
-let store = createStore(
-  rootReducer,
-  applyMiddleware(logger)
-);
+let store;
+if (process.env.NODE_ENV === 'development') {
+  store = createStore(
+    rootReducer,
+    applyMiddleware(logger)
+  );
+} else {
+  store = createStore(rootReducer);
+}
 
 export default store;
