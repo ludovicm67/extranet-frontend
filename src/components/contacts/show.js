b63d73167f67300dbd92d030cc69998a8b6162e1
ludovicm67 <ludovicmuller1@gmail.com>
2018-07-19 09:36:28 +0200

    contacts show is OK

--- src/components/contacts/show.js
+++ src/components/contacts/show.js
@@ -13,50 +13,50 @@ const styles = {
   formControl: {
     marginTop: '20px',
   },
+  line: {
+    display: 'block',
+  },
 };
 
 class ContactShow extends Component {
   state = {
     id: this.props.match.params.contactId,
-    firstname: '',
-    lastname: '',
-    role: '',
-    is_admin: 0,
-    email: '',
-    password: '',
+    name: '',
+    mail: '',
+    other: '',
+    type: 'Aucun',
   };
 
   componentDidMount() {
     getApi(`contacts/${this.state.id}`).then(res => {
       this.setState({
-        firstname: res.firstname || '',
-        lastname: res.lastname || '',
-        role: (res.is_admin === 1 ? (<strong>Super administrateur</strong>) : (res.role && res.role.name) ? res.role.name : 'Aucun'),
-        is_admin: res.is_admin,
-        email: res.email || '',
-        password: '',
+        name: res.name || '',
+        mail: res.mail || '',
+        other: res.other || '',
+        type: (res.type && res.type.name) || 'Aucun',
       });
     });
   }
 
   render() {
+    let other = this.state.other.split('\n').map((item, i) => <span key={i} style={styles.line}>{item}</span>);
     return (
       <div>
         <Typography variant="display1" gutterBottom>
-          {`${this.state.firstname} ${this.state.lastname} (${this.state.email})`}
+          {this.state.name}
         </Typography>
-        <Typography style={styles.intro}>Affichage des informations à propos de cet utilisateur</Typography>
+        <Typography style={styles.intro}>Affichage des informations à propos de ce contact</Typography>
         <Typography>
-          <strong>Nom complet : </strong>
-          {`${this.state.firstname} ${this.state.lastname}`}
+          <strong>Type : </strong>
+          {this.state.type}
         </Typography>
         <Typography>
           <strong>Adresse mail : </strong>
-          <a href={`mailto:${this.state.email}`}>{this.state.email}</a>
+          <a href={`mailto:${this.state.mail}`}>{this.state.mail}</a>
         </Typography>
         <Typography>
-          <strong>Rôle : </strong>
-          {this.state.role}
+          <strong>Autres informations : </strong>
+          {other}
         </Typography>
       </div>
     );
