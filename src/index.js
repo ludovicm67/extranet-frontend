7a07386ca280fae9e1fadd124faf07b22d906ecd
ludovicm67 <ludovicmuller1@gmail.com>
2018-07-19 10:00:25 +0200

    make webfont local

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'material-icons/iconfont/material-icons.css';
+import 'typeface-roboto';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store';
