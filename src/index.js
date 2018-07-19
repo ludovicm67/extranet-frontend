e14c043e7fcdbf69eefe1485eb01813f9d246f8a
ludovicm67 <ludovicmuller1@gmail.com>
2018-07-19 17:56:31 +0200

    register serviceworker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import 'material-icons/iconfont/material-icons.css';
 import 'typeface-roboto';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 import store from './store';
 import { Provider } from 'react-redux';
 import MuiPickersUtilsProvider from 'material-ui-pickers/utils/MuiPickersUtilsProvider';
@@ -27,4 +27,4 @@ const Root = (
 );
 
 ReactDOM.render(Root, document.getElementById('root'));
-// registerServiceWorker();
+registerServiceWorker();
