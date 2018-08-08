1c3e95bf9f2159a1c74345183d65138f1c99d824
ludovicm67 <ludovicmuller1@gmail.com>
2018-08-08 10:15:47 +0200

    started to work on permissions

--- src/components/roles/edit.js
+++ src/components/roles/edit.js
@@ -4,6 +4,13 @@ import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox';
 
 import { getApi, putApi } from '../../utils';
 
@@ -20,6 +27,7 @@ class RolesEdit extends Component {
   state = {
     id: this.props.match.params.roleId,
     name: '',
+    permissions : [],
   };
 
   componentDidMount() {
@@ -31,7 +39,20 @@ class RolesEdit extends Component {
         name: res.name,
       });
     });
+
+    getApi(`permissions/${this.state.id}`).then(res => {
+      if (this.isUnmounted) {
+        return;
+      }
+      this.setState({
+        permissions: res,
+      });
+    });
   }
+
+  handleCheckChange = name => event => {
+    this.setState({ [name]: event.target.checked });
+  };
 
   componentWillUnmount() {
     this.isUnmounted = true;
@@ -65,6 +86,70 @@ class RolesEdit extends Component {
             onChange={this.handleChange('name')}
           />
         </FormControl>
+        <Paper>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Nom</TableCell>
+                <TableCell>Afficher</TableCell>
+                <TableCell>Ajouter</TableCell>
+                <TableCell>Modifier</TableCell>
+                <TableCell>Supprimer</TableCell>
+                <TableCell>Tous</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.permissions.map((e, k) => (
+                <TableRow key={k}>
+                  <TableCell>{`${e.name}`}</TableCell>
+                  <TableCell>
+                    <Checkbox
+                      disabled={!e.show}
+                      checked={this.state.confidential}
+                      onChange={this.handleCheckChange(e.id, 'show')}
+                      value="1"
+                      color="primary"
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <Checkbox
+                      disabled={!e.add}
+                      checked={this.state.confidential}
+                      onChange={this.handleCheckChange(e.id, 'add')}
+                      value="1"
+                      color="primary"
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <Checkbox
+                      disabled={!e.edit}
+                      checked={this.state.confidential}
+                      onChange={this.handleCheckChange(e.id, 'edit')}
+                      value="1"
+                      color="primary"
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <Checkbox
+                      disabled={!e.delete}
+                      checked={this.state.confidential}
+                      onChange={this.handleCheckChange(e.id, 'delete')}
+                      value="1"
+                      color="primary"
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <Checkbox
+                      value="1"
+                      color="primary"
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+        {JSON.stringify(this.state.permissions)}
         <Button variant="contained" color="primary" style={styles.submit} onClick={this.handleSubmit.bind(this)}>
           Modifier
         </Button>
